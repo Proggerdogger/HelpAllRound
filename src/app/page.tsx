5a0b3e49fcd5e987006075bf62fd9182ce703035
Proggerdogger <143052591+Proggerdogger@users.noreply.github.com>
2025-05-06 23:01:48 +1000

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from "react";
+import Header from "@/app/header";
+import Slideshow from "@/app/slideshow";
+import Footer from "@/app/Footer";
+import ReasonsSection from "@/app/Reasons";
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Slideshow />
+      <ReasonsSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+ 
+    
+    //booking section and money section. 
+  
+
+    
+    
+
+    
+    
