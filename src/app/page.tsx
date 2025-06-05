77cd6579acbfdc708971782d12da496feaced753
Proggerdogger <143052591+Proggerdogger@users.noreply.github.com>
2025-06-05 20:43:49 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 // 'use client';
 
 import React from "react";
-// import Header from "@/app/header";
+
 import Slideshow from "@/app/slideshow";
-// import Footer from "@/app/Footer";
+
 import ReasonsSection from "@/app/Reasons";
 import type { Metadata } from "next";
-// import CallbackModal from '@/app/CallbackModal'; Commented out or remove
-// import CallbackSection from '@/app/CallbackSection'; // Import the new component
 
 export const metadata: Metadata = {
   title: "HelpAllRound | Home",
