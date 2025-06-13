33e19e4c81d4eb561d7b746f3e6e2c971e127100
Proggerdogger <143052591+Proggerdogger@users.noreply.github.com>
2025-06-13 12:02:31 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/Logo.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
